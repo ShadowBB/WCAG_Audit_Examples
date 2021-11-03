3d26e6ed2b7057682ac201d44aaf49e296231fa2
joshuaboele <Josboele@gmail.com>
2021-11-03 12:23:29 +0100

    Added index, added title/passed examples context switching. basic css

--- Version 2.0/app.js
+++ Version 2.0/app.js
@@ -1,4 +1,8 @@
+// define list element
+
 const list = document.querySelector('.criterium-list');
+
+// WCAG data
 
 const data = [
 
@@ -16,7 +20,7 @@ const data = [
 
             passed: [
                 {
-                    description: 'Image has a text alternative'
+                    description: '1.1.1 passed example'
                 },
             ],
 
@@ -32,16 +36,13 @@ const data = [
         examples: {
             failed: [
                 {
-                    description: 'This image has no text alternative'
-                },
-                {
-                    description: 'Usage of title is not allowed'
+                    description: 'Failed 1.2.1 example #1   '
                 },
             ],
 
             passed: [
                 {
-                    description: 'Image has a text alternative'
+                    description: '1.2.1 passed example'
                 },
             ],
 
@@ -54,14 +55,35 @@ const data = [
     },
 ];
 
+// dynamically generated list based on data
+
 const renderList = data.map((listItem, index) => {
     const listElement = document.createElement('li');
-    listElement.dataset.test = index;
+    listElement.dataset.chapter = index;
     const button = document.createElement('button');
+    button.classList.add('js-context-toggle');
     button.innerHTML = listItem.title;
     listElement.append(button)
     list.append(listElement);
 });
 
+// hooks
 
+const navigationButton = document.querySelectorAll('.js-context-toggle');
+const passedExample = document.querySelectorAll('.passed-example');
+const failedExample = document.querySelectorAll('.failed-example');
+const criterionName = document.querySelector('.criterion-name');
 
+// change content
+
+navigationButton.forEach(button => {
+    button.addEventListener('click', () => {
+        let index = button.parentElement.dataset.chapter;
+
+        console.log(failedExample)
+        console.log(passedExample[0]);
+        passedExample[0].innerHTML = data[index].examples.passed[0].description;
+        criterionName.innerHTML = data[index].title;
+
+    })
+})
