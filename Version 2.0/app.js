2e6d4a68fa2173746bf183e1b72bf9b21b606b0d
joshuaboele <Josboele@gmail.com>
2022-02-17 13:12:16 +0100

    Added dummy data, added codeblock support, template generation cleaned up

--- Version 2.0/app.js
+++ Version 2.0/app.js
@@ -5,10 +5,7 @@ const data = [
         examples: {
             failed: [
                 {
-                    description: 'This image has no text alternative'
-                },
-                {
-                    description: 'Usage of title is not allowed',
+                    description: ''
                 },
             ],
 
@@ -20,6 +17,12 @@ const data = [
             ],
 
             unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
                 {
                     description: 'Op deze pagina is dit niet duidelijk.... *link*'
                 },
@@ -44,6 +47,294 @@ const data = [
             unclear: [
                 {
                     description: ''
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.2.2 Captions (Prerecorded)',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.2.2 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.2.3 Audio Description or Media Alternative (Prerecorded)',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.2.3 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.2.4 Captions (Live)',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.2.4 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: '<pre><code><img src="afbeelding.jpg" alt="hond"></code></pre>'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.2.5 Audio Description (Prerecorded)',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.2.5 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.3.1 Info and Relationships',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.3.1 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: '<code>test</code>'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.3.2 Meaningful Sequence',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.3.2 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.3.3 Sensory Characteristics',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.3.3 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.3.4 Orientation',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.3.4 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+            ]
+        }
+    },
+    {
+        title: 'SC 1.3.5 Identify Input Purpose',
+        examples: {
+            failed: [
+                {
+                    description: 'This image has no text alternative'
+                },
+                {
+                    description: 'Usage of title is not allowed',
+                },
+            ],
+
+            passed: [
+                {
+                    description: '1.3.5 passed example',
+                    img: 'awdwada',
+                },
+            ],
+
+            unclear: [
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
+                },
+                {
+                    description: 'Op deze pagina is dit niet duidelijk.... *link*'
                 },
             ]
         }
--- Version 2.0/build.js
+++ Version 2.0/build.js
@@ -2,43 +2,49 @@
 const data = require('./app.js')
 const fs = require('fs');
 var nunjucks = require('nunjucks');
-nunjucks.configure('views', { autoescape: true });
+nunjucks.configure('views', { autoescape: false });
 
+let criterionNames = [];
 
-const generatePages = data.map((successCriterion, index) => {
-    console.log('---------------------------------')
-
-    // RegEx to replace empty spaces with dashes to comply with OS naming standards
-
-    const formattedTitle = successCriterion.title.replace(/ /g,"_")
-
-    // define arrays and objects
-
-    const failedExamplesArray = [];
-    const passedExamplesArray = [];
-    const unclearExamplesArray = [];
-
-    const failedExamples = successCriterion.examples.failed;
-    const passedExamples = successCriterion.examples.passed;
-    const unclearExamples = successCriterion.examples.unclear;
-
-    // Loop over each individual example type per succes criteria
-
-    unclearExamples.forEach(unclearExample => {
-        unclearExamplesArray.push(unclearExample.description);
-    })
-
-    passedExamples.forEach(passedExample => {
-        passedExamplesArray.push(passedExample.description)
-    })
-
-    failedExamples.forEach(failedExample => {
-        failedExamplesArray.push(failedExample.description);
-    })
-
-    // Pass the data to the index.html in order for nunjucks templating to work. Additionally replace the dist files on each node render command to generate new updated files that will be served to the user
-
-    var res = nunjucks.render('index.html', { title: successCriterion.title, failedExamples: failedExamplesArray, passedExamples: passedExamplesArray, unclearExamples: unclearExamplesArray});
-    fs.writeFileSync('./dist/' + formattedTitle + '.html', res);
+const listIndex = data.map((successCriterion) => {
+    criterionNames.push(successCriterion.title)
 });
 
+function Render(criterionNames) {
+    const generatePages = data.map((successCriterion) => {
+    
+        console.log('node js render')
+    
+        const formattedTitle = successCriterion.title;
+        
+        const failedExamplesArray = [];
+        const passedExamplesArray = [];
+        const unclearExamplesArray = [];
+    
+        const failedExamples = successCriterion.examples.failed;
+        const passedExamples = successCriterion.examples.passed;
+        const unclearExamples = successCriterion.examples.unclear;
+    
+        // Loop over each individual example type per succes criteria
+    
+        unclearExamples.forEach(unclearExample => {
+            unclearExamplesArray.push(unclearExample.description);
+        })
+    
+        passedExamples.forEach(passedExample => {
+            passedExamplesArray.push(passedExample.description)
+        })
+    
+        failedExamples.forEach(failedExample => {
+            failedExamplesArray.push(failedExample.description);
+        })
+    
+        // Pass the data to the index.html in order for nunjucks templating to work. Additionally replace the dist files on each node render command to generate new updated files that will be served to the user
+    
+        var res = nunjucks.render('index.html', { title: successCriterion.title, failedExamples: failedExamplesArray, passedExamples: passedExamplesArray, unclearExamples: unclearExamplesArray, criterionNames: criterionNames });
+        fs.writeFileSync('./dist/' + formattedTitle + '.html', res);
+    });
+    
+} 
+
+Render(criterionNames);
